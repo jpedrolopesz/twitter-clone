f2c280332b4d2ee8e755e6be50ba42a3bac9df36
jpedrolopesz <jplopeszamonelo@hotmail.com>
2023-01-30 19:29:36 -0300

    Initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+    <Head title="Home" />
+
+
+    <TwitterLayout>
+        <div class="text-white">
+
+            <div class="flex" v-for="tweet in tweets" :key="tweet">
+                <Tweet :tweet="tweet"/>
+            </div>
+            <div class="border-b border-b-gray-800 mt-2"></div>
+        </div>
+
+    </TwitterLayout>
+</template>
+
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import TwitterLayout from "@/Layouts/TwitterLayout.vue";
+import Tweet from "@/Components/Tweet.vue";
+
+defineProps({ tweets: Array })
+
+</script>
